a1648652a5f00a1585466d5a39377ba56431c52c
ThoonShweYiKyaw <thoonlay779@gmai.com>
2023-01-14 16:05:18 +0630

    14JAN

---
+++ admintemplateone/js/app.js
@@ -0,0 +1,11 @@
+// jQuery Area
+$(document).ready(function(){
+    // Start Left Sidebar Section
+    $('.sidebarlinks').click(function(){
+        $('.sidebarlinks').removeClass("currents");
+        $(this).addClass("currents");
+    });
+    // End Left Sidebar Section
+});
+
+// Javascript Area
